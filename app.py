2250663f3eeae32c9f9fa5e84306da20139a738d
alexjohn256 <alexjohn2562000@gmail.com>
2025-10-19 03:11:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,6 +39,10 @@ def compute():
     except Exception as e:
         return jsonify({'error': 'Computation error.'}), 500
 
-if __name__ == '__main__':
-    # debug mode for development; in production use a proper WSGI server
-    app.run(host='0.0.0.0', port=5000, debug=True)
+# if __name__ == '__main__':
+#     # debug mode for development; in production use a proper WSGI server
+#     app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
